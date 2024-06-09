b740c3b3c37142f33e081203019a6bd4043115ce
ivan <ivan767676@gmail.com>
2024-06-09 17:04:17 +0800

    1. refine admin show/edit/index view 2. fix cannot delete admin 3. modify manager picture 4. fix current_admin link error on right-top link

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,16 +1,16 @@
 class Admin < ActiveRecord::Base
     before_save :reset_super_admin
     before_update :reset_super_admin
-  
+
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable,
            :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:account]
-  
+
     validates_presence_of :account, :email
     validates_uniqueness_of :account, :case_sensitive => false
     validates_uniqueness_of :email, :case_sensitive => false
-  
+
     def type
       if self.super_admin
         I18n.t(:Super_Admin , scope: "Sidebar")
@@ -18,7 +18,7 @@ class Admin < ActiveRecord::Base
         I18n.t(:Admin , scope: "Sidebar")
       end
     end
-  
+
     def status
       if self.enable?
         I18n.t(:Active, scope: "Table")
@@ -26,19 +26,19 @@ class Admin < ActiveRecord::Base
         I18n.t(:Banned, scope: "Table")
       end
     end
-  
+
     def active_for_authentication?
       self.enable?
     end
-  
+
     def inactive_message
       "您的帳號目前停用中。"
     end
-    
+
     def default_name
       self.name || self.account.camelize
     end
-  
+
     def reset_super_admin
       if Admin.count == 0
         self.super_admin = true
@@ -48,6 +48,5 @@ class Admin < ActiveRecord::Base
         self.super_admin = false
       end
     end
-  
+
   end
-  
