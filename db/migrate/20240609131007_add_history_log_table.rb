41a4752c66fd8156dbd2d14eb194d5741efb5358
ivan <ivan767676@gmail.com>
2024-06-10 01:15:26 +0800

    add history_log_table

---
+++ db/migrate/20240609131007_add_history_log_table.rb
@@ -0,0 +1,17 @@
+class AddHistoryLogTable < ActiveRecord::Migration[7.1]
+  def change
+
+    create_table "history_logs", force: :cascade do |t|
+      t.integer  "source"
+      t.integer  "source_id"
+      t.integer  "target"
+      t.integer  "target_id"
+      t.integer  "action"
+      t.string   "data",                limit: 30, default: nil
+      t.string   "description",         limit: 50, default: nil
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+
+  end
+end
