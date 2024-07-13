4879af06839810dd6710cf54c6a8cd99ffc01d4f
ivan <ivan767676@gmail.com>
2024-07-13 01:33:40 +0000

    add user debug switch

--- app/controllers/application_web_controller.rb
+++ app/controllers/application_web_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationWebController < ApplicationController
 	before_action :authenticate_user!
+	#before_action :set_current_user
 
+	def set_current_user
+			@current_user = User.find(5)
+	end
 
 end
