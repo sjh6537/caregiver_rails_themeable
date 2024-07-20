b3ad7b38b06e9cabb2cb67fa1270f21f9e01b1fa
ivan <ivan767676@gmail.com>
2024-07-20 14:05:24 +0000

    show google map marker for admin shop show

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -15,4 +15,21 @@ class Shop < ActiveRecord::Base
       city + postal + self.address
     end
 
+    def show_text
+      if (self.is_show == true)
+        "顯示"
+      else
+        "不顯示"
+      end
+    end
+
+    def category_text
+      SHOP_CATEGORY_CODE.map do |c|
+        if self.category == c[:code]
+          return c[:category]
+        end
+      end
+        return "其他"
+    end
+
 end
