08197c87afc8a762398b05fa844c6758c28bc6de
Chenfun <chenfun168@gmail.com>
2025-05-11 09:07:53 +0800

    feat(health_report_crawler): 新增健康報告推播通知功能，包含照顧者通知

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,7 +1,7 @@
 module ReportHelper
-  def format_health_report(report)
-    text = "你有一份新的健康報告：\n\n"
-    text += "#{I18n.t('Table.Measure_Time')}: #{report.measure_time}\n"
+  def format_health_report(report, title = "你有一份新的健康報告：\n\n")
+    text = title
+    text += "#{I18n.t('Table.Measure_Time')}: #{report.measure_time.strftime('%Y-%m-%d %H:%M:%S')}\n"
     text += "#{I18n.t('Table.BMI')}: #{report.bmi}\n" if report.bmi.present?
     text += "#{I18n.t('Table.Weight')}: #{report.weight}\n" if report.weight.present?
     text += "#{I18n.t('Table.Temperature')}: #{report.temperature}\n" if report.temperature.present?
@@ -15,13 +15,12 @@ module ReportHelper
     text += "#{I18n.t('Table.Uric_Acid')}: #{report.uric_acid}\n" if report.uric_acid.present?
     text += "#{I18n.t('Table.Ketones')}: #{report.ketones}\n" if report.ketones.present?
     text += "#{I18n.t('Table.Total_Cholesterol')}: #{report.total_cholesterol}\n" if report.total_cholesterol.present?
-    text += "#{I18n.t('Table.Measure_Time')}: #{report.measure_time}\n" if report.measure_time.present?
     text
   end
 
-  def format_fitness_report(report)
-    text = "你有一份新的運動報告：\n\n"
-    text += "#{I18n.t('Table.Measure_Time')}: #{report.report_date}\n" if report.exercise_type.present?
+  def format_fitness_report(report, title = "你有一份新的運動報告：\n\n")
+    text = title
+    text += "#{I18n.t('Table.Measure_Time')}: #{report.report_date}\n" if report.report_date.present?
     text += "#{I18n.t('Table.Exercise_Type')}: #{report.exercise_type}\n" if report.exercise_type.present?
     text += "#{I18n.t('Table.Fitness_Device')}: #{report.fitness_device.name}\n" if report.fitness_device.present?
     text += "#{I18n.t('Table.Start_Time')}: #{report.start_time}\n" if report.start_time.present?
